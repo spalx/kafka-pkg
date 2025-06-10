4459d9647f83a8a2306fb5bc0ce07ab4a5917430
Artavazd Barseghyan <artavazd1@gmail.com>
2025-06-10 23:05:42 +0200

    remove dist

--- dist/src/common/correlated-kafka-request.d.ts
+++
@@ -1,8 +0,0 @@
-import { CorrelatedRequestDTO, CorrelatedResponseDTO } from '../types/correlated.dto';
-declare class CorrelatedKafkaRequest {
-    private pendingResponses;
-    private topic;
-    constructor(topic: string);
-    send(data: CorrelatedRequestDTO, timeout?: number): Promise<CorrelatedResponseDTO>;
-}
-export default CorrelatedKafkaRequest;
--- dist/src/common/correlated-kafka-response.d.ts
+++
@@ -1,8 +0,0 @@
-import { CorrelatedRequestDTO } from '../types/correlated.dto';
-declare class CorrelatedKafkaResponse {
-    private topic;
-    constructor(topic: string);
-    send(data: CorrelatedRequestDTO, error: unknown | null): Promise<void>;
-    private isErrorWithCode;
-}
-export default CorrelatedKafkaResponse;
